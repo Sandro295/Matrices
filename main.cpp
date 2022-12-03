2e3a77cd749037f7e90fafc4776c093fb37c92bc
Efimov Danil <danil.efimov.295@mail.ru>
2022-12-03 15:32:22 +0300

    Add multiplication via decomposed matricies

--- main.cpp
+++ main.cpp
@@ -3,7 +3,9 @@
 #include <random>
 #include "matrix.h"
 
-Matrix multiplyViaTensor(Matrix& a, Matrix& b, std::vector<Matrix>& tensor3d) {
+using Tensor = std::vector<Matrix>;
+
+Matrix multiplyViaTensor(Matrix& a, Matrix& b, Tensor& tensor3d) {
     Matrix result(a.Rows(), b.Columns());
     for (int m = 0; m < tensor3d.size(); ++m) {
         for (int i = 0; i < tensor3d[0].Rows(); ++i) {
@@ -18,6 +20,43 @@ Matrix multiplyViaTensor(Matrix& a, Matrix& b, std::vector<Matrix>& tensor3d) {
 }
 
 
+// u, v, w must have the same size
+
+// indexing is (row, column)
+Matrix multiplyDecomposed(const Matrix& a, const Matrix& b, const Matrix& u, const Matrix& v, const Matrix& w) {
+    Matrix c(a.Rows(), b.Columns());
+    std::vector<int> m(u.Columns());
+
+    for (int i = 0; i < u.Columns(); ++i) {
+        int a_sum = 0;
+        int b_sum = 0;
+        for (int j = 0; j < u.Rows(); ++j) {
+            a_sum += a(j, i) * u(j, i);
+            b_sum += b(j, i) * v(j, i);
+        }
+        m[i] = a_sum*b_sum;
+    }
+
+    std::vector<int> c_elements(a.Rows() * b.Columns())
+
+    for (int i = 0; i < w.Columns(); ++i) {
+        int m_sum = 0;
+        for (int j = 0; j < w.Rows(); ++j) {
+            m_sum += m[j] * w(j, i);
+        }
+        if (counter++ % a.Columns() != 0) {
+        }
+        c_elements[i] = m_sum;
+    }
+
+    for (int i = 0; i < a.Rows(); ++i) {
+        for (int j = 0; j < b.Columns(); ++j) {
+            c(j, i) = c_elements[b.Columns() * i + j];
+        }
+    }
+
+    return c;
+}
 
 void printMatrix(Matrix& a) {
     for (int i = 0; i < a.Rows(); ++i) {
@@ -43,8 +82,8 @@ void fillMatrixRandomly(Matrix& a) {
 }
 
 int main() {
-    Matrix a(5);
-    Matrix b(5);
+    Matrix a(2);
+    Matrix b(2);
 
     fillMatrixRandomly(a);
     fillMatrixRandomly(b);
@@ -53,6 +92,6 @@ int main() {
 
     printMatrix(regularMult);
 
-    // Matrix tensored = multiplyViaTensor();
+    // Matrix alphatensored = multiplyDecomposed(a, b);
 
 }
